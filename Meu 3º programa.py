44d0c0a8cc0a28ac17deaa4f76ea8614f6b10379
André Teles Cunha <andretc.2004@gmail.com>
2024-10-23 16:07:52 -0300

    1º Erro em python - descobrindo as diferenças do C
    Nesse programa tentei desenvolver de diversas formas um cálculo simples de fazer na linguagem C e não obtive sucesso pela diferença da semântica enre as linguagens.
    
    Estu salvando a minha frustação kkkkk.

---
+++ Meu 3º programa.py
@@ -0,0 +1,16 @@
+# Desenvolvendo o Desafio 3
+    # Autor: André Cunha
+
+# Interface do usuário
+print('==== Desafio 03 ====')
+
+# Declara e atribui valor às variáveis
+num_1 = input('Digite o 1º número: ')
+num_2 = input('Digite o 2º número: ')
+
+# Calcula a soma e atualiza o valor de num_1
+num_1 += num_2
+
+# Imprime o resultado
+print(num_1)
+
